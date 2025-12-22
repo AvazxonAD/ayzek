fad0fef7d9544e55120d731391201762d88f97d0
AvazxonAD <asqarovavazxon77@gmail.com>
2025-12-22 22:47:47 +0500

    jurnal

--- src/server.js
+++ src/server.js
@@ -13,7 +13,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+    allowedHeaders: "*",
+    exposedHeaders: "*",
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
