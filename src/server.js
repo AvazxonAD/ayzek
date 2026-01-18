0661da5b6fe4a9eebcaa23b7fdbe78ea3ac6ad6b
AvazxonAD <asqarovavazxon77@gmail.com>
2026-01-18 23:19:36 +0500

    ayzek

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
     allowedHeaders: "*",
     exposedHeaders: "*",
-  })
+  }),
 );
 app.options("*", cors());
 
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 
 // API Routes
-app.use("/", routes);
+app.use("/backend", routes);
 
 app.use(errorHandler);
 
